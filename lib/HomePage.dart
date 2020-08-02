0553fc2171c39ce1258eeb510110336815597612
nrj12 <neerajkv2000@gmail.com>
2020-08-02 16:03:03 +0530

    bug fixes

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
-
-import 'package:call_log/call_log.dart';
+import 'package:flutter/services.dart';
 import 'package:custom_switch/custom_switch.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -17,7 +16,7 @@ class _HomePaageState extends State<HomePaage> {
     Iterable<CallLogEntry> entries = await CallLog.get();
     var now = DateTime.now();
     int to = now.millisecondsSinceEpoch;
-    int from = now.subtract(Duration(days: 1)).millisecondsSinceEpoch;
+    int from = now.subtract(Duration(minutes: 30)).millisecondsSinceEpoch;
     entries =
         await CallLog.query(type: CallType.missed, dateFrom: from, dateTo: to);
 
@@ -212,4 +211,103 @@ class _HomePaageState extends State<HomePaage> {
       ),
     );
   }
+}
+
+class CallLog {
+  static const MethodChannel _channel =
+      const MethodChannel('sk.fourq.call_log');
+
+  /// Get all call history log entries. Permissions are handled automatically
+  static Future<Iterable<CallLogEntry>> get() async {
+    Iterable result = await _channel.invokeMethod('get', null);
+    return result?.map((m) => CallLogEntry.fromMap(m));
+  }
+
+  /// Query call history log entries
+  /// dateFrom: unix timestamp. precision in millis
+  /// dateTo: unix timestamp. precision in millis
+  /// durationFrom: minimal call length in seconds
+  /// durationTo: minimal call length in seconds
+  /// name: call participant name (present only if in contacts)
+  /// number: call participant phone number
+  /// type: value from [CallType] enum
+  static Future<Iterable<CallLogEntry>> query({
+    int dateFrom,
+    int dateTo,
+    int durationFrom,
+    int durationTo,
+    String name,
+    String number,
+    CallType type,
+    String numbertype,
+    String numberlabel,
+    String cachedNumberType,
+    String cachedNumberLabel,
+    String cachedMatchedNumber,
+  }) async {
+    var params = {
+      "dateFrom": dateFrom?.toString(),
+      "dateTo": dateTo?.toString(),
+      "durationFrom": durationFrom?.toString(),
+      "durationTo": durationTo?.toString(),
+      "name": name,
+      "number": number,
+      "type": type?.index == null ? null : (type.index + 1).toString(),
+      "cachedNumberType": cachedNumberType,
+      "cachedNumberLabel": cachedNumberLabel,
+      "cachedMatchedNumber": cachedMatchedNumber,
+    };
+    Iterable records = await _channel.invokeMethod('query', params);
+    return records?.map((m) => CallLogEntry.fromMap(m));
+  }
+}
+
+/// PODO for one call log entry
+class CallLogEntry {
+  CallLogEntry({
+    this.name,
+    this.number,
+    this.formattedNumber,
+    this.callType,
+    this.duration,
+    this.timestamp,
+    this.cachedNumberType,
+    this.cachedNumberLabel,
+  });
+
+  String name;
+  String number;
+  String formattedNumber;
+  CallType callType;
+  int duration;
+  int timestamp;
+  int cachedNumberType;
+  String cachedNumberLabel;
+  String cachedMatchedNumber;
+
+  CallLogEntry.fromMap(Map m) {
+    name = m['name'];
+    number = m['number'];
+    formattedNumber = m['formattedNumber'];
+    callType = m['callType'] < 1 || m['callType'] > 8
+        ? CallType.unknown
+        : CallType.values[m['callType'] - 1];
+    duration = m['duration'];
+    timestamp = m['timestamp'];
+    cachedNumberType = m['cachedNumberType'];
+    cachedNumberLabel = m['cachedNumberLabel'];
+    cachedMatchedNumber = m['cachedMatchedNumber'];
+  }
+}
+
+/// All possible call types
+enum CallType {
+  incoming,
+  outgoing,
+  missed,
+  voiceMail,
+  rejected,
+  blocked,
+  answeredExternally,
+  unknown,
 }
